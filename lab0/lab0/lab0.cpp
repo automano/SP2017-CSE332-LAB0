40da6d1759378030a401d99adf774c9759ca0ffb
zhening.li <zhening.li@wustl.edu>
2017-01-19 14:51:33 -0600

    first commit

---
+++ lab0/lab0/lab0.cpp
@@ -0,0 +1,22 @@
+// lab0.cpp
+// Author: Zhening Li E-mail:zhening.li@wustl.edu
+// Purpose: Lab0 main program entrance point
+
+
+#include "stdafx.h"
+#include "Header.h"
+#include "lab0.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	return SUCCESS;
+}
+
+int ParseFile(vector<string> &file_content, char *input_file_name)
+{
+
+}
+
