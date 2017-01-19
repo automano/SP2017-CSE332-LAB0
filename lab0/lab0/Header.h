71cf079ebc39719aed123fee06bdea396ccda538
zhening.li <zhening.li@wustl.edu>
2017-01-19 14:56:53 -0600

    add h files

---
+++ lab0/lab0/Header.h
@@ -0,0 +1,17 @@
+// Header.h
+// Author: Zhening Li E-mail: zhening.li@wustl.edu
+// Purpose: Declare an enumeration for the different array indices that are relevant to my program
+//          Declare an enumeration for the different success and failure values my program
+#pragma once
+
+enum ArrayIndices
+{
+	PROGRAM_NAME,
+	INPUT_FILE_NAME,
+	CDM_ARGUMENTS_NUMBER
+};
+
+enum ProgramStatus
+{
+	SUCCESS
+};
---
+++ lab0/lab0/lab0.h
@@ -0,0 +1,5 @@
+// lab0.h
+// Author: Zhening Li E-mail: zhening.li@wustl.edu
+// Purpose: Declare functions 
+#pragma once
+int ParseFile(vector<string> &file_content, char *input_file_name);
