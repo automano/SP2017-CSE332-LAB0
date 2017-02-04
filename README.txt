e59280d39a6c6e277696bce9718a67d92f7f1ff7
zhening.li <zhening.li@wustl.edu>
2017-02-04 13:42:07 -0600

    add README

---
+++ README.txt
@@ -0,0 +1,49 @@
+// Error
+http://stackoverflow.com/questions/21647/reading-from-text-file-until-eof-repeats-last-line#21666
+
+Reading from text file until EOF repeats last line.  
+Uses a ifstream object to read integers from a text file (which has one number per line) until it hits EOF. 
+But it read the content on the last line twice.
+
+// Extra Credit
+
+// command prompt
+
+SSH Secure Shell 3.2.9 (Build 283)
+Copyright (c) 2000-2003 SSH Communications Security Corp - http://www.ssh.com/
+
+This copy of SSH Secure Shell is a non-commercial version.
+This version does not include PKI and PKCS #11 functionality.
+
+
+Last failed login: Sat Feb  4 12:59:17 CST 2017 from rdsh1.seas.wustl.edu on ssh:notty
+There was 1 failed login attempt since the last successful login.
+Last login: Sat Feb 20 13:36:48 2016 from rdp-srv1.seasad.wustl.edu
+##### ATTENTION #####
+
+shell.cec is no longer suited for
+computationally-heavy tasks.
+
+Please use 'qlogin' to obtain a
+terminal on one of the LinuxLab
+hosts.
+
+Thank you!
+
+EIT
+
+#####
+[zhening.li@shell ~]$
+
+// Results of these trials
+
+[zhening.li@shell lab0]$ ./lab0.exe
+usage: ./lab0.exe <input_file_name>
+[zhening.li@shell lab0]$ ./lab0.exe input_file.txt 
+Non-digit characters: these all are strings 7string str4ing string2 
+Digit characters: 0 1 2 3 5 7 11 13 17 19 
+[zhening.li@shell lab0]$ 
+
+
+
+
--- lab0/lab0/lab0.cpp
+++ lab0/lab0/lab0.cpp
@@ -10,16 +10,56 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int main(int argc, char * argv[])
 {
 	const int expected_argument_count = 2;
-	if (argc == expected_argument_count) // correct arguments (exactly one argument)
+	if (argc == expected_argument_count) // correct arguments (exactly one argument and the program name)
 	{
 		char * file_name = argv[INPUT_FILE_NAME];
 		vector<string> file_content;
-		return ParseFile(file_content, file_name);
+		vector<int> file_integer;
+		if (ParseFile(file_content, file_name) == SUCCESS)
+		{
+			cout <<"Non-digit characters: ";
+			for (vector<string>::const_iterator i = file_content.begin(); i != file_content.end(); ++i)
+			{
+				string temp;
+				temp = *i;
+				int is_not_digit = 0;
+				for (int n = 0;n < temp.size();n++)
+				{
+					if (!isdigit(temp[n])) // Adapted from http://www.cplusplus.com/reference/cctype/isdigit/
+					{
+						is_not_digit++;
+					}
+				}
+				if (is_not_digit == 0)
+				{// For each string that contains only numeric digit character
+					stringstream ss(temp);
+					int x;
+					ss >> x;
+					file_integer.push_back(x);
+				}
+				else
+				{// For each string that contains any other (non-digit) characters, print the string to the standard output stream.
+					
+					cout << temp << " ";
+				}
+			}
+			cout << endl<<"Digit characters: ";
+			for (vector<int>::const_iterator i = file_integer.begin(); i != file_integer.end(); ++i)
+			{
+				cout << *i << " ";
+			}
+			return SUCCESS;
+		}
+		else
+		{
+			return UNABLE_TO_PARSE_FILE;
+		}
 	}
 	else // wrong arguments number
 	{
@@ -41,11 +81,8 @@ int ParseFile(vector<string> &file_content, char *input_file_name)
 			if(myfile.eof()) break; //adapted from http://stackoverflow.com/questions/21647/reading-from-text-file-until-eof-repeats-last-line#21666s
 			file_content.push_back(temp);
 		} while (!myfile.eof());
-		myfile.close();
+		myfile.close(); // END 
 		
-		//for (vector<string>::const_iterator i = file_content.begin(); i != file_content.end(); ++i)
-			//cout << *i << ' ';
-
 		return SUCCESS;
 	}
 	else
